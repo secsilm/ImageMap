3c68ce55c0ee59b60725521c58c3a98c8797986a
secsilm <secsilm@outlook.com>
2017-03-16 16:11:27 +0800

    修复命令行参数不能用的bug

--- classify.py
+++ classify.py
@@ -3,31 +3,35 @@ import os
 import logging
 import click
 
-@click.command()
-@click.option('--imgdir', help='存放着图片的目录，同时新的分类目录也将在这个目录下')
-
 logging.basicConfig(level=logging.ERROR)
 
-# 存放着图片的目录，同时新的分类目录也将在这个目录下
-base_path = r'C:\Users\secsi\Pictures\Saved Pictures\图片地图测试'
-fileformats = ['jpg', 'jpeg']
-it = os.scandir(base_path)
-for entry in it:
-    if entry.name.split('.')[-1] in fileformats and entry.is_file():
-        logging.info(entry.name)
-        address = locate(entry.path)
-        if address is None:
-            logging.warning('{} 未知地点'.format(entry.name))
-            unknown_path = os.path.join(base_path, '未知地点')
-            os.makedirs(unknown_path, exist_ok=True)
-            os.rename(entry.path, os.path.join(unknown_path, entry.name))
-            continue
-        logging.debug(address)
-        if address.province == address.city:
-            # 按照国家、省份、城市、区分类，并创建目录 
-            path = os.path.join(base_path, address.country, address.city, address.district)
-        else:
-            path = os.path.join(base_path, address.city, address.district)
-        os.makedirs(path, exist_ok=True)
-        os.rename(entry.path, os.path.join(path, entry.name))
-        print(entry.name, address, path)
+@click.command()
+@click.option('--imgdir', help='存放着图片的目录，同时新的分类目录也将在这个目录下')
+def classify(imgdir):
+    base_path = imgdir
+    fileformats = ['jpg', 'jpeg']
+    it = os.scandir(base_path)
+    for entry in it:
+        if entry.name.split('.')[-1] in fileformats and entry.is_file():
+            logging.info(entry.name)
+            address = locate(entry.path)
+            # 如果没有得到GPS信息则将其分到未知地点目录
+            if address is None:
+                logging.warning('{} 未知地点'.format(entry.name))
+                unknown_path = os.path.join(base_path, '未知地点')
+                os.makedirs(unknown_path, exist_ok=True)
+                os.rename(entry.path, os.path.join(unknown_path, entry.name))
+                continue
+            logging.debug(address)
+            if address.province == address.city:
+                # 按照国家、省份、城市、区分类，并创建目录 
+                path = os.path.join(base_path, address.country, address.city, address.district)
+            else:
+                path = os.path.join(base_path, address.city, address.district)
+            os.makedirs(path, exist_ok=True)
+            os.rename(entry.path, os.path.join(path, entry.name))
+            logging.info(entry.name, address, path)
+
+
+if __name__ == '__main__':
+    classify()

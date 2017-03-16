43b2a6c4a85922ea6e5b7ebaab86078cdaa96455
secsilm <secsilm@outlook.com>
2017-03-16 21:03:13 +0800

    增加可以单独查看一个照片的地址信息的功能

--- classify.py
+++ classify.py
@@ -1,37 +1,57 @@
-from locate import locate
+from locate import locate, get_address
 import os
 import logging
 import click
 
 logging.basicConfig(level=logging.ERROR)
 
+
+def only_show_address(imgpath, detail=True):
+    '''只显示地址信息
+    detail: bool, 是否显示详细的地址信息
+    '''
+
+    address = locate(imgpath, detail)
+    logging.debug(address, detail)
+    if detail:
+        print(imgpath, address.formatted_address, address.sematic_description)
+    else:
+        print(imgpath, address.formatted_address)
+
+
 @click.command()
-@click.option('--imgdir', help='存放着图片的目录，同时新的分类目录也将在这个目录下')
-def classify(imgdir):
-    base_path = imgdir
-    fileformats = ['jpg', 'jpeg']
-    # 使用 os.scandir() 要比 os.listdir() 更有效率
-    it = os.scandir(base_path)
-    for entry in it:
-        if entry.name.split('.')[-1] in fileformats and entry.is_file():
-            logging.info(entry.name)
-            address = locate(entry.path)
-            # 如果没有得到GPS信息则将其分到未知地点目录
-            if address is None:
-                logging.warning('{} 未知地点'.format(entry.name))
-                unknown_path = os.path.join(base_path, '未知地点')
-                os.makedirs(unknown_path, exist_ok=True)
-                os.rename(entry.path, os.path.join(unknown_path, entry.name))
-                continue
-            logging.debug(address)
-            if address.province == address.city:
-                # 按照国家、省份、城市、区分类，并创建目录 
-                path = os.path.join(base_path, address.country, address.city, address.district)
-            else:
-                path = os.path.join(base_path, address.city, address.district)
-            os.makedirs(path, exist_ok=True)
-            os.rename(entry.path, os.path.join(path, entry.name))
-            logging.info(entry.name, address, path)
+@click.option('--class_/--no-class_', default=True, help='是否分类图片到不同的文件夹，默认为True')
+@click.option('--imgpath', help='存放着图片的目录，同时新的分类目录也将在这个目录下')
+def classify(class_, imgpath):
+    if class_ and os.path.isdir(imgpath):
+        base_path = imgpath
+        fileformats = ['jpg', 'jpeg']
+        # 使用 os.scandir() 要比 os.listdir() 更有效率
+        it = os.scandir(base_path)
+        for entry in it:
+            if entry.name.split('.')[-1] in fileformats and entry.is_file():
+                logging.info(entry.name)
+                address = locate(entry.path)
+                # 如果没有得到GPS信息则将其分到未知地点目录
+                if address is None:
+                    logging.warning('{} 未知地点'.format(entry.name))
+                    unknown_path = os.path.join(base_path, '未知地点')
+                    os.makedirs(unknown_path, exist_ok=True)
+                    os.rename(entry.path, os.path.join(unknown_path, entry.name))
+                    continue
+                logging.debug(address)
+                if address.province == address.city:
+                    # 按照国家、省份、城市、区分类，并创建目录 
+                    path = os.path.join(base_path, address.country, address.city, address.district)
+                else:
+                    path = os.path.join(base_path, address.city, address.district)
+                os.makedirs(path, exist_ok=True)
+                os.rename(entry.path, os.path.join(path, entry.name))
+                logging.info(entry.name, address, path)
+    elif os.path.isfile(imgpath):
+        only_show_address(imgpath, detail=True)
+    else:
+        print('请检查输入的 classify_ 和 imgpath 是否匹配！')
 
 
 if __name__ == '__main__':

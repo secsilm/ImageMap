e5cde9acb7d6800cf6de4274270c029ba06fdc8c
Alan Lee <secsilm@outlook.com>
2017-03-16 15:05:30 +0800

    实现按地点分类

--- classify.py
+++ classify.py
@@ -1,3 +1,29 @@
 from locate import locate
 import os
+import logging
 
+logging.basicConfig(level=logging.ERROR)
+
+# 存放着图片的地址，同时新的分类目录也将在这个目录下
+base_path = r'C:\Users\secsi\Pictures\Saved Pictures\图片地图测试'
+fileformats = ['jpg', 'jpeg']
+it = os.scandir(base_path)
+for entry in it:
+    if entry.name.split('.')[-1] in fileformats and entry.is_file():
+        logging.info(entry.name)
+        address = locate(entry.path)
+        if address is None:
+            logging.warning('{} 未知地点'.format(entry.name))
+            unknown_path = os.path.join(base_path, '未知地点')
+            os.makedirs(unknown_path, exist_ok=True)
+            os.rename(entry.path, os.path.join(unknown_path, entry.name))
+            continue
+        logging.debug(address)
+        if address.province == address.city:
+            # 按照国家、省份、城市、区分类，并创建目录 
+            path = os.path.join(base_path, address.country, address.city, address.district)
+        else:
+            path = os.path.join(base_path, address.city, address.district)
+        os.makedirs(path, exist_ok=True)
+        os.rename(entry.path, os.path.join(path, entry.name))
+        print(entry.name, address, path)

b7aae87a114ea0582ec4c86968b34ab43a80660b
secsilm <secsilm@outlook.com>
2017-03-17 20:00:48 +0800

    修复坐标重合时标记显示异常的bug

--- classify.py
+++ classify.py
@@ -2,8 +2,10 @@ from locate import locate, get_address
 import os
 import logging
 import click
+import gmplot
+from numpy import mean
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def only_show_address(imgpath, detail=True):
@@ -12,6 +14,11 @@ def only_show_address(imgpath, detail=True):
     '''
 
     address = locate(imgpath, detail)
+    # 用于绘制在地图上的坐标
+    lat, lng = address.google_latlng.split(',')
+    gmap = gmplot.GoogleMapPlotter(float(lat), float(lng), zoom=15)
+    gmap.heatmap([float(lat)], [float(lng)], radius=15, opacity=1)
+    gmap.draw('imgmap.html')
     logging.debug('address: {}, detail: {}'.format(address, detail))
     if detail:
         print(imgpath, address.formatted_address, address.sematic_description)
@@ -20,13 +27,18 @@ def only_show_address(imgpath, detail=True):
 
 
 @click.command()
-@click.option('--class_/--no-class_', default=True, help='是否分类图片到不同的文件夹，默认为True')
+@click.option('--class_/--no-class_', default=True, help='是否分类图片到不同的文件夹，默认为 True')
+@click.option('--showmap/--no-showmap', default=True, help='将照片在地图上标注出来，默认为 False')
 @click.option('--imgpath', help='存放着图片的目录，同时新的分类目录也将在这个目录下')
-def classify(class_, imgpath):
-    if class_ and os.path.isdir(imgpath):
+def classify(class_, imgpath, showmap):
+    if class_ and os.path.isdir(imgpath) and showmap:
         logging.debug('class_: {}, isdir: {}'.format(class_, os.path.isdir(imgpath)))
         base_path = imgpath
         fileformats = ['jpg', 'jpeg']
+
+        # 用于绘制在地图上的坐标
+        lats = []
+        lngs = []
         # 使用 os.scandir() 要比 os.listdir() 更有效率
         it = os.scandir(base_path)
         for entry in it:
@@ -40,7 +52,13 @@ def classify(class_, imgpath):
                     os.makedirs(unknown_path, exist_ok=True)
                     os.rename(entry.path, os.path.join(unknown_path, entry.name))
                     continue
+                
                 logging.debug(address)
+
+                lat, lng = address.google_latlng.split(',')
+                lats.append(float(lat))
+                lngs.append(float(lng))
+
                 if address.province == address.city:
                     # 按照国家、省份、城市、区分类，并创建目录 
                     path = os.path.join(base_path, address.country, address.city, address.district)
@@ -49,6 +67,20 @@ def classify(class_, imgpath):
                 os.makedirs(path, exist_ok=True)
                 os.rename(entry.path, os.path.join(path, entry.name))
                 logging.info('entry name: {}, address: {}, path: {}'.format(entry.name, address, path))
+        
+        latlng_set = set()
+        for i, j in zip(lats, lngs):
+            latlng_set.add((i, j))
+        lats, lngs = zip(*latlng_set)
+        center_lat = (max(lats) + min(lats)) / 2
+        center_lng = (max(lngs) + min(lngs)) / 2
+
+        logging.debug('center_lat: {}, center_lng: {}'.format(center_lat, center_lng))
+        logging.debug('lats: {}, lngs: {}'.format(lats, lngs))
+
+        gmap = gmplot.GoogleMapPlotter(center_lat, center_lng, zoom=10)
+        gmap.heatmap(lats, lngs, radius=15, opacity=1, dissipating=True)
+        gmap.draw('imgmap.html')
     elif not class_ and os.path.isfile(imgpath):
         logging.debug('class_: {}, isdir: {}'.format(class_, os.path.isdir(imgpath)))
         only_show_address(imgpath, detail=True)
